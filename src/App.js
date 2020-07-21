5e5311500f1e81c7f0e90b552eb9c74811872a41
Bruno Fávero <bruno.2956@gmail.com>
2020-07-21 10:59:23 -0300

    changed language to pt

--- src/App.js
+++ src/App.js
@@ -27,11 +27,11 @@ function App() {
 
   return (
     < Container className="my-4" >
-      <h1 className="mb-4" >GitHub Jobs</h1>
+      <h1 className="mb-4" >Empregos GitHub</h1>
       < SearchForm params={params} onParamChange={paramChangeHandler} />
       <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
-      {loading && <h1>Loading ...</h1> }
-      {error && <h1>Error. Try Refresing.</h1> }
+      {loading && <h1>Carregando ...</h1> }
+      {error && <h1>Erro. Tente recarregar a página.</h1> }
       {jobs.map(job => {
         return <Job key={job.id} job={job} />
       })}
--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -9,11 +9,11 @@ const SearchForm = ({params, onParamChange}) => {
         <Form className="mb-4">
             <Form.Row className="align-items-end" >
                 <Form.Group as={Col}>
-                    <Form.Label>Description</Form.Label>
+                    <Form.Label>Descrição</Form.Label>
                     <Form.Control onChange={onParamChange} value={params.description} name="description"></Form.Control>
                 </Form.Group>
                 <Form.Group as={Col}>
-                    <Form.Label>Location</Form.Label>
+                    <Form.Label>Lugar</Form.Label>
                     <Form.Control onChange={onParamChange} value={params.location} name="location"></Form.Control>
                 </Form.Group>
                 <Form.Group as={Col} xs="auto" className="ml-2">
